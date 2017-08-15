ae610176f8039884e85798697ddfa3ff27aba39d
Sven SAULEAU <sven.sauleau@xtuc.fr>
2017-08-15 22:05:55 +0200

    feat: split into packages

---
+++ packages/bytearena-sdk/src/index.js
@@ -0,0 +1,3 @@
+export number from './number';
+export vector from 'bytearena-sdk-vector2';
+export comm from 'bytearena-sdk-comm';
--- src/browser/index.js
+++
@@ -1,5 +0,0 @@
-import * as _number from '../number';
-import { Vector2 } from '../vector2';
-
-export const vector = { Vector2 };
-export const number = _number;

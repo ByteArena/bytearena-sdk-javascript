4199e4a02b59a384b979f3e870a34d3656420c6e
Jérôme Schneider <contact@netgusto.com>
2017-12-30 17:40:20 +0100

    WIP: High level API

--- packages/bytearena-sdk/src/index.js
+++ packages/bytearena-sdk/src/index.js
@@ -1,5 +1,6 @@
 import * as number from 'bytearena-sdk-math';
 import * as vector from 'bytearena-sdk-vector2';
 import * as comm from 'bytearena-sdk-comm';
+import * as highlevel from 'bytearena-sdk-highlevel';
 
-export {number, vector, comm};
+export { number, vector, comm, highlevel};
